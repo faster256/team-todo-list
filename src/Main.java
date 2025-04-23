4a467b9771ae81a09a20adae12b7b6c3b5067d46
faster256 <firstoneno@gmail.com>
2025-04-23 15:25:37 +0300

    Сортування по даті додавання
    Додав сортування по даті додавання

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import utils.FileReader;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,9 +13,24 @@ public class Main {
 
         fileReader.readFile(todos);
 
-        // Сортування за часом створення задачі
-        todos.stream()
-                .sorted(Comparator.comparing(Task::getCreatedAtAsDateTime))
-                .forEach(todo -> System.out.println(todo.getValue()));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Виберіть варіант сортування:");
+        System.out.println("1 - За датою додавання до списку");
+        System.out.println("2 - За часом створення задачі");
+
+        int choice = scanner.nextInt();
+
+        if (choice == 1) {
+            // Сортування за датою додавання (за замовчуванням порядок у списку)
+            todos.forEach(todo -> System.out.println(todo.getValue()));
+        } else if (choice == 2) {
+            // Сортування за часом створення задачі
+            todos.stream()
+                    .sorted(Comparator.comparing(Task::getCreatedAtAsDateTime))
+                    .forEach(todo -> System.out.println(todo.getValue()));
+        } else {
+            System.out.println("Невірний вибір. Список буде виведений без сортування.");
+            todos.forEach(todo -> System.out.println(todo.getValue()));
+        }
     }
 }
