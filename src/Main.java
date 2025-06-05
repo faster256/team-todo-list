cf69486bc6d591210b626d87152dfd497bf89bf5
faster256 <firstoneno@gmail.com>
2025-06-05 13:11:11 +0300

    Додати функціонал завершення програми

--- src/Main.java
+++ src/Main.java
@@ -14,25 +14,38 @@ public class Main {
         fileReader.readFile(todos);
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Виберіть опцію:");
-        System.out.println("1 - Вивести список (за датою додавання)");
-        System.out.println("2 - Вивести список (за часом створення)");
-        System.out.println("3 - Зберегти список у файл");
+        boolean running = true;
 
-        int choice = scanner.nextInt();
+        while (running) {
+            System.out.println("\nВиберіть опцію:");
+            System.out.println("1 - Вивести список (за датою додавання)");
+            System.out.println("2 - Вивести список (за часом створення)");
+            System.out.println("3 - Зберегти список у файл");
+            System.out.println("0 - Вихід");
 
-        if (choice == 1) {
-            todos.forEach(todo -> System.out.println(todo.getValue()));
-        } else if (choice == 2) {
-            todos.stream()
-                    .sorted(Comparator.comparing(Task::getCreatedAtAsDateTime))
-                    .forEach(todo -> System.out.println(todo.getValue()));
-        } else if (choice == 3) {
-            FileWriterUtil writer = new FileWriterUtil("./src/data/todos_saved.txt");
-            writer.writeToFile(todos);
-        } else {
-            System.out.println("Невірний вибір. Список буде виведений без сортування.");
-            todos.forEach(todo -> System.out.println(todo.getValue()));
+            System.out.print("Ваш вибір: ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    todos.forEach(todo -> System.out.println(todo.getValue()));
+                    break;
+                case 2:
+                    todos.stream()
+                            .sorted(Comparator.comparing(Task::getCreatedAtAsDateTime))
+                            .forEach(todo -> System.out.println(todo.getValue()));
+                    break;
+                case 3:
+                    FileWriterUtil writer = new FileWriterUtil("./src/data/todos_saved.txt");
+                    writer.writeToFile(todos);
+                    break;
+                case 0:
+                    running = false;
+                    System.out.println("Програму завершено.");
+                    break;
+                default:
+                    System.out.println("Невірний вибір. Повторіть спробу.");
+            }
         }
     }
 }
