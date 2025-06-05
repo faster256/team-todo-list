27c4479142f0263708cbf5428b9c7aba5d7bef86
faster256 <firstoneno@gmail.com>
2025-06-05 13:13:01 +0300

    Додати функціонал виведення вмісту файлу

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import schemas.Task;
 import utils.TaskFileReader;
 import utils.FileWriterUtil;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -21,6 +24,7 @@ public class Main {
             System.out.println("1 - Вивести список (за датою додавання)");
             System.out.println("2 - Вивести список (за часом створення)");
             System.out.println("3 - Зберегти список у файл");
+            System.out.println("4 - Вивести вміст файлу як текст");
             System.out.println("0 - Вихід");
 
             System.out.print("Ваш вибір: ");
@@ -39,6 +43,9 @@ public class Main {
                     FileWriterUtil writer = new FileWriterUtil("./src/data/todos_saved.txt");
                     writer.writeToFile(todos);
                     break;
+                case 4:
+                    printFileContents("./src/data/todos.txt");
+                    break;
                 case 0:
                     running = false;
                     System.out.println("Програму завершено.");
@@ -46,6 +53,20 @@ public class Main {
                 default:
                     System.out.println("Невірний вибір. Повторіть спробу.");
             }
+        }
+    }
+
+    private static void printFileContents(String filePath) {
+        System.out.println("\nВміст файлу " + filePath + ":");
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(filePath));
+            if (lines.isEmpty()) {
+                System.out.println("[Файл порожній]");
+            } else {
+                lines.forEach(System.out::println);
+            }
+        } catch (IOException e) {
+            System.out.println("Не вдалося прочитати файл: " + filePath);
         }
     }
 }
